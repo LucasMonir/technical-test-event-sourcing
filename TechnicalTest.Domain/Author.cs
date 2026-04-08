b2a55d31180fa7d820bdcda269e8ba148acb5458
LucasM-Ateli <lucas.monir@dobby.be>
2026-04-08 10:48:34 -0300

    wip: add domain classes

---
+++ TechnicalTest.Domain/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalTest.Domain
+{
+    public class Author
+    {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+    }
+}
---
+++ TechnicalTest.Domain/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalTest.Domain
+{
+    public class Post
+    {
+        public required Guid Id { get; set; }
+        public required Guid AuthorId { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Content { get; set; }
+    }
+}
