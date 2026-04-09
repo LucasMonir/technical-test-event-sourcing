a482b9f5e0b709e221f7bb8eafc9b64d6815ecbd
LucasM-Ateli <lucas.monir@dobby.be>
2026-04-09 15:13:02 -0300

    wip: add E2E test for post controller get posts

---
+++ TechnicalTest.E2E.Test/E2ETests/PostGetE2ETests.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Json;
+using TechnicalTest.Application.DTOs;
+using TechnicalTest.Infrastructure.Persistence;
+using TechnicalTest.TestHelpers.Builders;
+
+namespace TechnicalTest.E2E.Test.E2ETests
+{
+    public class PostGetE2ETests : IClassFixture<TechnicalTestWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        private readonly AppDbContext _dbContext;
+
+        public PostGetE2ETests(TechnicalTestWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+
+            var scope = factory.Services.CreateScope();
+            _dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        }
+
+
+        [Fact]
+        public async Task GetPostIncludingAuthorReturnsPostWithAuthor()
+        {
+            var author = AuthorBuilder.Default()
+                .Build();
+
+            var post = PostBuilder.Default()
+                .WithAutorId(author.Id)
+                .Build();
+
+            AuthorDto authorDto = GetAuthorDto(author);
+            PostDto postDto = GetPostDto(post, authorDto);
+
+            _dbContext.Authors.Add(author);
+            _dbContext.Posts.Add(post);
+
+            await _dbContext.SaveChangesAsync();
+
+            var response = await _client.GetAsync($"/post/{post.Id}?includeAuthor=true");
+
+            var result = await response.Content.ReadFromJsonAsync<PostDto>();
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(postDto);
+        }
+
+        [Fact]
+        public async Task GetPostIncludingAuthorReturnsPostWithoutAuthor()
+        {
+            var author = AuthorBuilder.Default()
+                .Build();
+
+            var post = PostBuilder.Default()
+                .WithAutorId(author.Id)
+                .Build();
+
+            AuthorDto authorDto = GetAuthorDto(author);
+            PostDto postDto = GetPostDto(post, authorDto);
+
+            _dbContext.Authors.Add(author);
+            _dbContext.Posts.Add(post);
+
+            await _dbContext.SaveChangesAsync();
+
+            var response = await _client.GetAsync($"/post/{post.Id}");
+
+            var result = await response.Content.ReadFromJsonAsync<PostDto>();
+
+            result.Should().NotBeNull();
+            result.Author.Should().BeNull();
+            result.Should().BeEquivalentTo(result, options =>
+                options.Excluding(post => post.Author)
+            );
+        }
+
+        private static PostDto GetPostDto(Domain.Post post, AuthorDto authorDto)
+            => new(post.Id, post.AuthorId, post.Title, post.Description, post.Content, authorDto);
+
+        private static AuthorDto GetAuthorDto(Domain.Author author)
+        => new(author.Id, author.Name, author.Surname);
+    }
+}
---
+++ TechnicalTest.E2E.Test/TechnicalTestWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using TechnicalTest.Infrastructure.Persistence;
+
+namespace TechnicalTest.E2E.Test
+{
+    public class TechnicalTestWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        private SqliteConnection? connection;
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlite(connection));
+
+                using var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureCreated();
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                connection?.Close();
+                connection?.Dispose();
+            }
+        }
+    }
+}
