80a91de62fdc963a7c192171be4065144e3bab31
LucasM-Ateli <lucas.monir@dobby.be>
2026-04-08 22:49:14 -0300

    test: add WithNoExistingPost repository test for Post repository

---
+++ TechnicalTest.Infrastructure.Test/Repositories/WithPostRepository/WhenGetPostAsync/WithNoExistingPost.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using TechnicalTest.Infrastructure.Persistence.Repositories;
+
+namespace TechnicalTest.Infrastructure.Persistence.Test.Repositories.WithPostRepository.WhenGetPostAsync
+{
+    public class WithNoExistingPost
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly PostRepository _postRepository;
+        private readonly SqliteConnection _connection;
+        private readonly Guid _id;
+
+        public WithNoExistingPost()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _appDbContext = new AppDbContext(options);
+            _appDbContext.Database.EnsureCreated();
+
+            _postRepository = new PostRepository(_appDbContext);
+        }
+
+        [Fact]
+        public async Task ThenMustReturnNull()
+        {
+            var result = await _postRepository.GetPostAsync(_id);
+
+            result.Should().BeNull();
+
+            DisposeAsync();
+        }
+
+        private async void DisposeAsync()
+        {
+            await _appDbContext.DisposeAsync();
+            _connection.Close();
+        }
+    }
+}
